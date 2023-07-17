bb6971415e623140fc9584e86df3c0874652c858
unknown <princekumar170901@gmail.com>
2023-07-18 00:04:47 +0530

    register page updated

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     # other URL patterns
     path('', views.dash, name='dash'),
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register, name='register'),
+    path('signup', views.signup, name='signup'),
+    path('register', views.register, name='register'),
+    path('log', views.log, name='log'),
     # other URL patterns
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,29 +1,29 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+# from .models import user
 
 def dash(request):
     return render(request, 'dash.html')
 
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            if user.is_superuser:
-                # Admin login successful
-                login(request, user)
-                return redirect('/admin')  # Redirect to the admin dashboard
-            else:
-                # User login successful
-                login(request, user)
-                return redirect('user_dashboard')  # Redirect to the user dashboard
-        else:
-            # Invalid credentials
-            return render(request, 'login.html', {'error': 'Invalid credentials'})
-
-    return render(request, 'login.html')
-
+def log(request):
+    return render(request, 'log.html')
 
 def register(request):
     return render(request,'register.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+            name=request.POST['name']
+            email=request.POST['email']
+            password=request.POST['password']
+            phone=request.POST['phone']
+            address=request.POST['address']
+
+            user1= User.objects.create_user(name=name, email=email, password=password, phone=phone,address=address)
+            print(user1)
+            print('user created')
+            return redirect('/')
+    
+    else:
+         return render(request,'signup.html')
