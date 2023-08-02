e8372df15386b908c6a9812b7025665d2e8b57cd
princekumar1709 <princekumar170901@gmail.com>
2023-08-02 15:30:30 +0530

    setup visualization port

--- authentication/views.py
+++ authentication/views.py
@@ -3,11 +3,6 @@ from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from .models import ExcelData 
 import pandas as pd
-<<<<<<< HEAD
-from.models import ExcelData
-=======
-
->>>>>>> 47427bd16bb8d411586f223128308ac0f83a4198
 # from .models import user
 
 
@@ -76,17 +71,6 @@ def visual(request):
 
         # Read the Excel file into a pandas DataFrame
         df = pd.read_excel(excel_file)
-<<<<<<< HEAD
-
-        columns = df.columns.to_list()
-
-        # Convert DataFrame to JSON (List of dictionaries)
-        data_list = df.to_dict(orient='records')
-
-        # Save data to the database
-        ExcelData.objects.create(data=data_list)
-        return render(request, 'visual.html',{'cols':columns})
-=======
         print(df)
         cols=df.columns.to_list()
         
@@ -99,6 +83,5 @@ def visual(request):
         # Save data to the database
         ExcelData.objects.create(data=data_list)
         return render(request,'visual.html',{'columns':cols,'data':data_list})  
->>>>>>> 47427bd16bb8d411586f223128308ac0f83a4198
     else:
         return redirect(request,'upload')
